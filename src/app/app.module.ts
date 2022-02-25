b0ce871a25c7fc4676915245fb8d00e761df48b6
Mari-Artho <mari.artho@medieinstitutet.se>
2022-02-25 17:44:42 +0100

    Add files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ComfirmComponent } from './components/comfirm/comfirm.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    CheckoutComponent,
+    ComfirmComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
