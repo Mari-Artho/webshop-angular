fde0f96d4962ea2461332f77e4d03b54e5f1f8ea
Mari-Artho <mari.artho@medieinstitutet.se>
2022-03-01 15:37:32 +0100

    Put location button back from stash.

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { IProduct } from 'src/app/models/IProduct';
 import { ProductService } from 'src/app/service/product.service';
 
@@ -12,7 +13,7 @@ export class DetailComponent implements OnInit {
   movieId:string = '';
   products: IProduct[] = [];
 
-  constructor(private route: ActivatedRoute, private service: ProductService) { }
+  constructor(private route: ActivatedRoute, private location: Location, private service: ProductService) { }
 
   //There was a problem with 'null', so I added ' || "" '.
   ngOnInit(): void {
@@ -27,6 +28,10 @@ export class DetailComponent implements OnInit {
     this.products = this.service.productInfo;
   } 
 
+  //Back to previous page.
+  backBtn(){
+    this.location.back();
+  }
 }
 
 
