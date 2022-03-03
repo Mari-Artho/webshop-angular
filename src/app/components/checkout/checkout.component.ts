d98c676cbfd20549b2dcc8c941d0fdd249cf7628
Mari-Artho <mari.artho@medieinstitutet.se>
2022-03-03 21:15:35 +0100

    Create checkout table and fetch id data.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/service/cart.service';
+import { IProduct } from 'src/app/models/IProduct';
 
 @Component({
   selector: 'app-checkout',
@@ -9,11 +10,25 @@ import { CartService } from 'src/app/service/cart.service';
 export class CheckoutComponent implements OnInit {
   items = this.service.items;
   
-  constructor(
-    private service: CartService,
-    ) { }
+  constructor (private service: CartService,) { }
     
   ngOnInit(): void {
+  }
+
+  //Remove Cart
+  remove(item:IProduct){
+    for (let i = 0; i<this.items.length; i++){
+      if( this.items[i] == item) {
+        this.items.splice(i ,1);
+        break;
+      }
+    }
+    this.save();
+  }
+
+  //Save Cart Items.
+  private save() {
+    localStorage.setItem('cart', JSON.stringify(this.items));
   }
 
 }
